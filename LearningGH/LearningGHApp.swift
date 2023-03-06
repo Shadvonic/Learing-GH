b9ceaa31700b34a7c65f2bbac83559628fdd3ef0
Shadvonic <moxeymarcus28@gmail.com>
2023-03-06 13:36:00 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Marc Moxey on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
